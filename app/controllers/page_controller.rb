ec944813cef031d69a87b698bf40c1ab7de22e46
jli <jli@github.com>
2014-09-09 11:12:57 +0800

    Short url in upper case

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,8 +2,11 @@ class PageController < ActionController::Base
   
   def index
     if params[:code].blank?
+      # The short url is not valid
+      render :text => "This is not a valid URL, please check the URL you are supposed to visit and try again."
     else
-      url = Url.find_by shorturl: params[:code]
+      url = Url.find_by shorturl: params[:code].upcase
+      
       if url
         redirctUrl = url.originalurl
         
@@ -12,6 +15,9 @@ class PageController < ActionController::Base
         end
         
         redirect_to redirctUrl
+      else
+        # The short url is not valid
+        render :text => "This is not a valid URL, please check the URL you are supposed to visit and try again."
       end
     end 
   end
--- app/controllers/shorturl_controller.rb
+++ app/controllers/shorturl_controller.rb
@@ -61,7 +61,7 @@ class ShorturlController < ActionController::Base
   end
   
   def generateCode(len)
-    chars = ("a".."z").to_a + ("0".."9").to_a + ("A".."Z").to_a
+    chars =  ("A".."Z").to_a + ("0".."9").to_a
     newpass = ""
     1.upto(len) { |i| newpass << chars[rand(0...chars.size)] }
     return newpass
