04cac3c51f5edbba584c6dbabbe07ac2cecd50dc
jli <jli@github.com>
2014-09-16 13:59:58 +0800

    redirect via js

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -8,13 +8,11 @@ class PageController < ActionController::Base
       url = Url.find_by shorturl: params[:code].upcase
       
       if url
-        redirctUrl = url.originalurl
+        @redirctUrl = url.originalurl
         
-        if !redirctUrl.start_with?('http://') && !redirctUrl.start_with?('https://')
-          redirctUrl = 'http://' + redirctUrl
+        if !@redirctUrl.start_with?('http://') && !@redirctUrl.start_with?('https://')
+          @redirctUrl = 'http://' + @redirctUrl
         end
-        
-        redirect_to redirctUrl
       else
         # The short url is not valid
         render :text => "This is not a valid URL, please check the URL you are supposed to visit and try again."
