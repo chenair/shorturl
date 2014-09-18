7530019258c3af6bcbc3cfcd8d9bcdb486d84c7b
jli <jli@github.com>
2014-09-18 13:58:48 +0800

    change domain to qa

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module ShortURL
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.ent_url_base = 'http://shorturlstage.herokuapp.com/'
+    config.ent_url_base = 'http://buy.qa.entertainment.com/'
   end
 end
